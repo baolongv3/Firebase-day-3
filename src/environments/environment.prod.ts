6bcbc11f7f46cb75535e84083c32e3abadb387e2
baolongv3 <43814741+baolongv3@users.noreply.github.com>
2021-04-07 23:31:04 +0700

    Init repo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+	  apiKey: "AIzaSyB3L876TXsXdthUzT_4F1_ga7swBMgaQxQ",
+	  authDomain: "realtime-collab.firebaseapp.com",
+	  databaseURL: "https://realtime-collab-default-rtdb.firebaseio.com",
+	  projectId: "realtime-collab",
+	  storageBucket: "realtime-collab.appspot.com",
+	  messagingSenderId: "792699906631",
+	  appId: "1:792699906631:web:76b83d0fb979d459d59f62",
+	  measurementId: "G-2S6X1TBPPS"
+	}
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+	  apiKey: "AIzaSyB3L876TXsXdthUzT_4F1_ga7swBMgaQxQ",
+	  authDomain: "realtime-collab.firebaseapp.com",
+	  databaseURL: "https://realtime-collab-default-rtdb.firebaseio.com",
+	  projectId: "realtime-collab",
+	  storageBucket: "realtime-collab.appspot.com",
+	  messagingSenderId: "792699906631",
+	  appId: "1:792699906631:web:76b83d0fb979d459d59f62",
+	  measurementId: "G-2S6X1TBPPS"
+	}
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
